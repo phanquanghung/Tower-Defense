9c94f2f09ea337906f244cf71bc902913793aad9
heor2807 <47893091+heor2807@users.noreply.github.com>
2019-10-24 11:25:44 +0700

    Immovable
    Update interface cho immovable.

---
+++ game/src/Entity/Immoveable/TowerExtend/MachineGunTower.java
@@ -0,0 +1,9 @@
+package Entity.Immoveable.TowerExtend;
+
+import Entity.Immoveable.Tower;
+
+public class MachineGunTower extends Tower {
+    protected MachineGunTower (double posX, double posY, double width, double height) {
+        super(posX, posY, width, height);
+    }
+}
---
+++ game/src/Entity/Immoveable/TowerExtend/SniperTower.java
@@ -0,0 +1,9 @@
+package Entity.Immoveable.TowerExtend;
+
+import Entity.Immoveable.Tower;
+
+public class SniperTower extends Tower {
+    protected SniperTower (double posX, double posY, double width, double height) {
+        super(posX, posY, width, height);
+    }
+}
