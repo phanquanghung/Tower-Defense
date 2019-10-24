d99a66759378221fbe4eaa9008da9a3b1f4bbf57
manhdung <manhdung20112000@gmail.com>
2019-10-24 17:01:43 +0700

    Revert "loadMap function done!"
    This reverts commit 6948639e206da809da9ae767fd618e94b72fdbb6.

--- game/Map/towerDefenseDemo.tsx
+++ game/Map/towerDefenseDemo.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.5" name="towerDefenseDemo" tilewidth="64" tileheight="64" tilecount="299" columns="23">
- <image source="../../game assets/Tilesheet/towerDefense_tilesheet.png" width="1472" height="832"/>
+ <image source="../game assets/Tilesheet/towerDefense_tilesheet.png" width="1472" height="832"/>
 </tileset>
