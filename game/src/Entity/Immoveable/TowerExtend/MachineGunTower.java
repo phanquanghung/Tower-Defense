5d5e0a1603bfa375899aef730ad5490ce001e78b
heor2807 <47893091+heor2807@users.noreply.github.com>
2019-11-11 10:13:12 +0700

    Tower

--- game/src/Entity/Immoveable/TowerExtend/MachineGunTower.java
+++ game/src/Entity/Immoveable/TowerExtend/MachineGunTower.java
@@ -1,7 +1,38 @@
 package Entity.Immoveable.TowerExtend;
 
 import Entity.Immoveable.Tower;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 
 public class MachineGunTower implements Tower {
 
+    @Override
+    public void setImg(Image image) {
+
+    }
+
+    @Override
+    public void draw(GraphicsContext gc) {
+
+    }
+
+    @Override
+    public Image getImg() {
+        return null;
+    }
+
+    @Override
+    public double getPosX() {
+        return 0;
+    }
+
+    @Override
+    public double getPosY() {
+        return 0;
+    }
+
+    @Override
+    public void setPosXY(double posX, double posY) {
+
+    }
 }
